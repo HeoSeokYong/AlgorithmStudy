b4f2fe510d5d6997358893cf67b0aea86aa57e3e
HeoSeokYong <hsdragoner@gmail.com>
2022-10-01 19:19:03 +0900

    22-10-01 study

---
+++ DFS/pipe_guy.py
@@ -0,0 +1,47 @@
+# 백준 #16724 피리 부는 사나이
+'''
+    Algorithm: 그래프 탐색(DFS?), 분리 집합
+    시간복잡도: O(2*N*M) = 약 2,000,000
+'''
+import sys
+
+input = sys.stdin.readline
+direction = {'U': (-1, 0), 'D': (1, 0), 'R': (0, 1), 'L': (0, -1)}
+
+def solution(N, M, maps):
+    '''
+        maps의 방문하지 않은 모든 곳을 탐색.
+        group_num: 그룹 구분을 위한 변수
+        visited: 기본값 0 에서 방문시 group_num 값으로 path별 그룹 분리
+        answer: path가 자기 자신으로 돌아온 경우 safe zone +1 추가,
+                다른 그룹과 만난 경우 두 그룹은 이어진 것이므로 answer 변화 없음
+    '''
+    answer, group_num = 0, 0
+    visited = [[0] * M for _ in range(N)]
+
+    for i in range(N):
+        for j in range(M):
+            if visited[i][j] == -1:
+                x, y = i, j
+                group_num += 1
+
+                while True:
+                    visited[x][y] = group_num
+                    
+                    dx, dy = direction[maps[x][y]]
+                    nx, ny = x + dx, y + dy
+
+                    if not visited[nx][ny]:
+                        x, y = nx, ny
+                    else:
+                        if visited[nx][ny] == group_num:
+                            answer += 1
+                        break
+                
+    return answer
+
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    maps = [input() for _ in range(N)]
+
+    print(solution(N, M, maps))
---
+++ Shortest_Path/prepare_meeting.py
@@ -0,0 +1,172 @@
+# # 백준 #2610 회의 준비
+# '''
+#     Algorithm: 큐
+#     시간복잡도: O(N^2)?
+
+# import sys
+# from collections import deque
+
+# input = sys.stdin.readline
+# INF = 1e2
+
+# def friends_committee(friends):
+#     '''
+#         committees: set들의 list
+#     '''
+#     committees = []
+#     belong = [False] * (len(friends)+1)  
+
+#     for i in range(1, len(friends)+1):
+#         deq = deque()
+#         comm = set()
+
+#         if not belong[i]:
+#             deq.append(i)
+#             belong[i] = True
+
+#             while deq:
+#                 x = deq.popleft()
+#                 comm.add(x)
+
+#                 for friend in friends[x]:
+#                     if not belong[friend]:
+#                         belong[friend] = True
+#                         deq.append(friend)
+
+#             committees.append(comm)
+
+#     return committees
+
+# def elect_representative(friends, committee):
+#     '''
+#         모든 참석자들의 의사전달시간 중 최댓값이 최소가 되도록 대표를 정함
+#     '''
+#     time = INF
+#     representative = 0
+
+#     for c in committee:
+#         cmt_dict = {cmt: [False, 0] for cmt in committee}
+
+#         deq = deque([(c, 0)])
+#         cmt_dict[c][0] = True
+        
+#         while deq:
+#             x, t = deq.popleft()
+
+#             for friend in friends[x]:
+#                 if not cmt_dict[friend][0]:
+#                     cmt_dict[friend][1] = t+1
+#                     cmt_dict[friend][0] = True
+#                     deq.append((friend, t+1))
+        
+#         max_comm_time = max(cmt_dict.values(), key=lambda x:x[1])[1]
+#         if time > max_comm_time:
+#             time = max_comm_time
+#             representative = c
+
+#     return representative
+
+# def solution(friends):
+#     committees = friends_committee(friends)
+
+#     return sorted([elect_representative(friends, committee) for committee in committees])
+
+
+# if __name__ == "__main__":
+#     N = int(input())
+#     M = int(input())
+
+#     friends = {i: [] for i in range(1, N+1)}
+#     for _ in range(M):
+#         a, b = map(int, input().split())
+#         friends[a].append(b)
+#         friends[b].append(a)
+
+#     K = solution(friends)
+
+#     print(len(K))
+#     for k in K:
+#         print(k)
+
+### **************************************************************
+
+'''
+    Algorithm: 플로이드-워셜 
+    시간복잡도: O(N^3) = 약 1,000,000 
+'''
+import sys
+
+input = sys.stdin.readline
+INF = 1e2
+
+def floyd_warshall(N, friends):
+    dist = [[INF] * (N+1) for _ in range(N+1)]
+
+    for i in range(1, N+1):
+        dist[i][i] = 0
+    
+    for i in range(1, N+1):
+        for j in friends[i]:
+            dist[i][j] = 1
+    
+    for k in range(1, N+1):
+        for i in range(1, N+1):
+            for j in range(1, N+1):
+                if dist[i][j] > dist[i][k] + dist[k][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+
+    return dist
+
+def friends_committee(dist):
+    committees = []
+    belong = [False] * (len(dist))  
+
+    for i in range(1, len(dist)):
+        comm = []
+        if not belong[i]:
+            for j in range(len(dist)):
+                if dist[i][j] != INF:
+                    belong[j] = True
+                    comm.append(j)
+
+            committees.append(comm)
+
+    return committees
+
+def elect_representative(dist, committee):
+    '''
+        모든 참석자들의 의사전달시간 중 최댓값이 최소가 되도록 대표를 정함
+    '''
+    time = INF
+    representative = 0
+
+    for c in committee:
+        max_comm_time = max([d for d in dist[c] if d < INF])
+        if time > max_comm_time:
+            time = max_comm_time
+            representative = c
+    
+    return representative
+
+def solution(N, friends):
+    dist = floyd_warshall(N, friends)
+    committees = friends_committee(dist)
+    
+    return sorted([elect_representative(dist, committee) for committee in committees])
+
+
+if __name__ == "__main__":
+    N = int(input())
+    M = int(input())
+
+    friends = {i: [] for i in range(1, N+1)}
+    for _ in range(M):
+        a, b = map(int, input().split())
+        friends[a].append(b)
+        friends[b].append(a)
+
+    K = solution(N, friends)
+
+    print(len(K))
+    for k in K:
+        print(k)
