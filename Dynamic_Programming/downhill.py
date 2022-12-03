aa4d45929b84aced75592fc4c5134fca940ba65a
HeoSeokYong <hsdragoner@gmail.com>
2022-12-03 23:21:41 +0900

    2022-12-03 study

--- Dynamic_Programming/downhill.py
+++ Dynamic_Programming/downhill.py
@@ -1,33 +1,34 @@
 # 백준 #1520 내리막 길
 '''
-    Algorithm: DP
-    Time Complexity:
+    Algorithm: DP, dfs
+    Time Complexity: -
 
+    dfs로 탐색을 하며 dp에 정보를 기록해 중복 탐지를 방지.
 '''
 import sys
 
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 dxdy = ((0, 1), (1, 0), (0, -1), (-1, 0))
 
 def solution(M, N):
     maps = [list(map(int, input().split())) for _ in range(M)]
-    dp = [[0] * N for _ in range(M)]
+    dp = [[-1] * N for _ in range(M)]
 
-    for i in range(M):
-        for j in range(N):
-            if i == 0 and j == 0:
-                dp[i][j] = 1
-            else:
-                for di, dj in dxdy:
-                    ni, nj = i + di, j + dj
+    def dfs(x, y):
+        if x == M-1 and y == N-1:
+            return 1
+        
+        if dp[x][y] == -1:
+            dp[x][y] = 0
+            for dx, dy in dxdy:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < M and 0 <= ny < N and maps[x][y] > maps[nx][ny]:
+                    dp[x][y] += dfs(nx, ny)
 
-                    if 0 <= ni < M and 0 <= nj < N and maps[i][j] > maps[ni][nj]:
-                        dp[ni][nj] += 1
+        return dp[x][y]
 
-    for d in dp:
-        print(d)
-
-    return
+    return dfs(0, 0)
 
 
 if __name__ == "__main__":
