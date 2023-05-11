042dc44bbc9a812147b9b1a4bd649c65054c29eb
HeoSeokYong <hsdragoner@gmail.com>
2023-05-11 11:41:38 +0900

    2023-05-11 study

---
+++ DFS/tree.py
@@ -0,0 +1,47 @@
+# 백준 #1068 트리
+'''
+    Algorithm: dfs, tree
+    Time Complexity: O(N)
+
+'''
+import sys
+
+from typing import List, Tuple, Callable
+
+
+def input() -> Callable:
+    return sys.stdin.readline().rstrip()
+
+
+def read_data() -> Tuple:
+    N = int(input())
+    parents = list(map(int, input().split()))
+    del_node = int(input())
+    return N, parents, del_node
+
+
+def solution(N:int, parents:List[int], del_node:int) -> int:
+    result = 0
+    stack = []
+    tree = [[] for _ in range(N)]
+
+    for i in range(N):
+        if parents[i] == -1:
+            stack.append(i)
+        elif i != del_node:
+            tree[parents[i]].append(i)
+
+    while stack:
+        node = stack.pop()
+
+        if not tree[node]:
+            result += 1
+
+        for n in tree[node]:
+            stack.append(n)
+
+    return result
+
+
+if __name__ == "__main__":
+    print(solution(*read_data()))
